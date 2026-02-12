ccaa7de085b810f0f0ccc669259e0f141730a6df
ahmadtaheri1991 <ahmadtaheri1991@gmail.com>
2026-02-12 21:43:24 +0330

    Add login functionality with error handling and loading state

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,41 @@
+"use client";
+
+import { SubmitEvent, useState } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Login() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function login(e: SubmitEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      const response = await fetch("https://dummyjson.com/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const res = await response.json();
+
+      if (!response.ok) {
+        setErrorMessage(res.message);
+        return;
+      }
+
+      router.push("/");
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
     <div className="bg-[#F9F9FC] min-h-screen">
       <div className="flex flex-col justify-center max-w-[448px] mx-auto py-8">
@@ -13,8 +50,10 @@ export default function Login() {
             "shadow-[0px_1px_2px_0px_color-mix(in_srgb,#111128_10%,transparent)]",
           ].join(" ")}
         >
-          <form>
+          <form onSubmit={login}>
             <div className="flex flex-col gap-4">
+              <div className="text-red-600 text-center">{errorMessage}</div>
+              
               <input
                 placeholder="Enter your username"
                 className={[
@@ -22,6 +61,8 @@ export default function Login() {
                   "rounded-md border border-[#e4e4ee]",
                   "focus:border-2 focus:outline-none focus:border-[#6B46C1]",
                 ].join(" ")}
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
               />
 
               <input
@@ -31,13 +72,15 @@ export default function Login() {
                   "rounded-md border border-[#e4e4ee]",
                   "focus:border-2 focus:outline-none focus:border-[#6B46C1]",
                 ].join(" ")}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
 
               <button
                 type="submit"
                 className="rounded-md cursor-pointer bg-[#6B46C1] text-white h-10 font-semibold"
               >
-                Continue with email
+                {loading ? "..." : "Continue with email"}
               </button>
             </div>
           </form>
