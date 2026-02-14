d0b25f9849d557101bc88d0c85d257e9ad87deb8
ahmadtaheri1991 <ahmadtaheri1991@gmail.com>
2026-02-14 21:14:05 +0330

    Refactor "Select All" checkbox in DropdownSelect component to include label and field wrapper for improved accessibility and layout

--- app/ui/dropdown.tsx
+++ app/ui/dropdown.tsx
@@ -3,6 +3,8 @@
 import {
   Input,
   Checkbox,
+  Field,
+  Label,
   Listbox,
   ListboxButton,
   ListboxOption,
@@ -122,13 +124,20 @@ export default function DropdownSelect(props: Props) {
             />
 
             {multiple && (
-              <Checkbox
-                checked={isAllChecked}
-                onChange={handleIsAllCheckedChange}
-                className="inline-block group size-6 rounded-md bg-white/10 p-1 ring-1 ring-white/15 ring-inset focus:not-data-focus:outline-none data-checked:bg-white data-focus:outline data-focus:outline-offset-2 data-focus:outline-white"
-              >
-                <CheckIcon className="hidden size-4 fill-black group-data-checked:block" />
-              </Checkbox>
+              <Field className="flex items-center gap-2">
+                <Checkbox
+                  checked={isAllChecked}
+                  onChange={handleIsAllCheckedChange}
+                  className="inline-block group size-6 rounded-md bg-white/10 p-1 ring-1 ring-white/15 ring-inset focus:not-data-focus:outline-none data-checked:bg-white data-focus:outline data-focus:outline-offset-2 data-focus:outline-white"
+                >
+                  <CheckIcon className="hidden size-4 fill-black group-data-checked:block" />
+                </Checkbox>
+
+                <Label className="text-gray-400 text-sm">
+                  {!selected.length ? "No" : selected.length} item
+                  {selected.length > 1 && "s"} selected
+                </Label>
+              </Field>
             )}
 
             {!groupedItems &&
