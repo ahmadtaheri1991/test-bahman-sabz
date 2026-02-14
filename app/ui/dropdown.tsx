9d56e45182f6ad1df236687c6f02b130d0a435de
ahmadtaheri1991 <ahmadtaheri1991@gmail.com>
2026-02-14 16:56:15 +0330

    Refactor DropdownSelect component to accept dynamic items and selection handling via props

--- app/ui/dropdown.tsx
+++ app/ui/dropdown.tsx
@@ -8,29 +8,34 @@ import {
 } from "@headlessui/react";
 import { CheckIcon, ChevronDownIcon } from "@heroicons/react/20/solid";
 import clsx from "clsx";
-import { useState } from "react";
 
-const people = [
-  { id: 1, name: "Tom Cook" },
-  { id: 2, name: "Wade Cooper" },
-  { id: 3, name: "Tanya Fox" },
-  { id: 4, name: "Arlene Mccoy" },
-  { id: 5, name: "Devon Webb" },
-];
+interface Item {
+  id: number;
+  name: string;
+}
 
-export default function DropdownSelect() {
-  const [selected, setSelected] = useState(people[1]);
+interface Props {
+  items: Item[];
+  selected: null | Item;
+  onSelectionChange: (item: Item) => void;
+}
+
+export default function DropdownSelect(props: Props) {
+  const { items, selected, onSelectionChange } = props;
 
   return (
     <div className="mx-auto h-screen w-52 pt-20">
-      <Listbox value={selected} onChange={setSelected}>
+      <Listbox
+        value={selected || { id: -1, name: "Select item" }}
+        onChange={onSelectionChange}
+      >
         <ListboxButton
           className={clsx(
-            "relative block w-full rounded-lg bg-black/90 py-1.5 pr-8 pl-3 text-left text-sm/6 text-white",
+            "relative block w-full min-h-9 rounded-lg bg-black/90 py-1.5 pr-8 pl-3 text-left text-sm/6 text-white",
             "focus:not-data-focus:outline-none data-focus:outline-2 data-focus:-outline-offset-2 data-focus:outline-black/75",
           )}
         >
-          {selected.name}
+          {selected?.name}
 
           <ChevronDownIcon
             className="group pointer-events-none absolute top-2.5 right-2.5 size-4 fill-white/60"
@@ -46,15 +51,15 @@ export default function DropdownSelect() {
             "transition duration-100 ease-in data-leave:data-closed:opacity-0",
           )}
         >
-          {people.map((person) => (
+          {items.map((item) => (
             <ListboxOption
-              key={person.name}
-              value={person}
-              className="group flex cursor-default items-center gap-2 rounded-lg px-3 py-1.5 select-none data-focus:bg-black/80"
+              key={item.name}
+              value={item}
+              className="group flex cursor-default items-center gap-2 rounded-lg px-3 py-1.5 select-none data-focus:bg-white/10"
             >
               <CheckIcon className="invisible size-4 fill-white group-data-selected:visible" />
 
-              <div className="text-sm/6 text-white">{person.name}</div>
+              <div className="text-sm/6 text-white">{item.name}</div>
             </ListboxOption>
           ))}
         </ListboxOptions>
